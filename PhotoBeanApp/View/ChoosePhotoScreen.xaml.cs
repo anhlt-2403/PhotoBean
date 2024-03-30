31d598d813d863bf44c413f49084c750d51559f3
tuananh-ngx <145064771+tuananh-ngx@users.noreply.github.com>
2024-03-30 12:56:50 +0700

    adjust ui v2

--- PhotoBeanApp/View/ChoosePhotoScreen.xaml.cs
+++ PhotoBeanApp/View/ChoosePhotoScreen.xaml.cs
@@ -56,8 +56,8 @@ namespace PhotoBeanApp.View
 
         private void SetUpLeftGrid()
         {
-            double columnWidth = 250;
-            double rowHeight = 200;
+            double columnWidth = ChoosePhoto.ActualWidth / numberOfColumns;
+            double rowHeight = ChoosePhoto.ActualHeight / numberOfRows;
             ChoosePhoto.ColumnDefinitions.Clear();
             ChoosePhoto.RowDefinitions.Clear();
 
@@ -94,8 +94,8 @@ namespace PhotoBeanApp.View
         }
         private void SetUpRightGrid()
         {
-            double columnWidth = 200;
-            double rowHeight = 200;
+            double columnWidth = Photos.ActualWidth / numberOfColumns;
+            double rowHeight = Photos.ActualHeight / (numberOfRows + 1);
 
             for (int i = 0; i < numberOfColumns; i++)
             {
