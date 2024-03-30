bbde7d51e8fbaae98052e4fa8d2c712f44dd03bb
tuananh-ngx <145064771+tuananh-ngx@users.noreply.github.com>
2024-03-30 16:13:54 +0700

    Adjust UI

---
+++ PhotoBeanApp/Helper/Classes/AddWidthConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PhotoBeanApp.Helper.Classes
+{
+    public class AddWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length == 2 && values[0] is double leftWidth && values[1] is double rightWidth)
+            {
+                return leftWidth + rightWidth - (leftWidth + rightWidth) / 5 * 2;
+            }
+
+            return 200;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
